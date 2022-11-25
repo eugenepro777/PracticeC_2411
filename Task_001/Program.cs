433c9b376275b0b5f8c8affb3d4fd16481a0477a
Eugene_Pro <proskuryakov.e@list.ru>
2022-11-25 10:45:17 +0300

    update solve Task_001

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -1,35 +1,50 @@
 ﻿// За каждый месяц банк начисляет к сумме вклада 7% от суммы.
-// Напишите консольную программу, в которую пользователь вводит сумму вклада
-// и количество месяцев.
-// А банк вычисляет конечную сумму вклада с учетом начисления процентов за каждый месяц.
-
+// Напишите консольную программу, в которую пользователь вводит сумму 
+// вклада и количество месяцев.
+// А банк вычисляет конечную сумму вклада с учетом начисления
+//  процентов за каждый месяц.
 // Для вычисления суммы с учетом процентов используйте цикл for.
 // Для ввода суммы вклада используйте
 // выражение Convert.ToDecimal(Console.ReadLine())
 // (сумма вклада будет представлять тип decimal) 
 
 Console.WriteLine("Введите сумму вклада: ");
-decimal amount = Convert.ToDecimal(Console.ReadLine());
+decimal depositSumm = Convert.ToDecimal(Console.ReadLine());
 
 Console.WriteLine("Введите длительность вклада: ");
 int month = Convert.ToInt32(Console.ReadLine());
 
-int percent = 7;
+int percent = 7; //процент вклада устанавливаем
+
+PercentPrint(depositSumm);
 
 decimal PercentSearch(decimal num1, int num2)
 {
     decimal result = num1;
     for (int i = 0; i < num2; i++)
     {
-        result = result + result * percent / 100;
+        result = result + result * percent / 100;//нарастающим итогом
     }
     return result;
 }
-if(amount > 0)
+
+// if(sum > 0)
+// {
+//     Console.WriteLine($"{PercentSearch(depositSumm, month):N2}");
+// }
+// else
+// {
+//     Console.WriteLine("Вклад нулевой");
+// }
+
+void PercentPrint(decimal sum)
 {
-    Console.WriteLine(PercentSearch(amount, month));
-}
-else
-{
-    Console.WriteLine("Вклад нулевой");
+    if(sum > 0)
+    {
+        Console.WriteLine($"{PercentSearch(depositSumm, month):N2}");
+    }
+    else
+    {
+        Console.WriteLine("Вклад нулевой");
+    }
 }
---
+++ Task_002_v2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит на консоль таблицу умножения.
+// Реализовать через массив и метод, используя цикл foreach
+
+int[] array = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+void Table(int[] arrayOut)
+{
+    foreach (int i in arrayOut)
+    {
+        foreach (int j in arrayOut)
+        {
+            int result = i * j;
+            Console.Write("{0}x{1}={2}\t", i, j, result);
+        }
+        Console.WriteLine();
+    }
+}
+
+Table(array);
